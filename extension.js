4cc9839cc17769989b09374f10b270cc4456846d
chenziyun <chenziyun@qbmisrv.com>
2020-12-24 09:55:47 +0800

    删除无用代码

--- extension.js
+++ extension.js
@@ -12,22 +12,6 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "nouveau-rich" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('nouveau-rich.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from nouveau-rich!');
-	});
-
-	context.subscriptions.push(disposable);
-
 	let configuration = vscode.workspace.getConfiguration().get("rich");
 	let url = "https://api.binance.com/api/v3/ticker/price";
 	let filter = configuration.symbols;
